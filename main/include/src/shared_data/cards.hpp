ad71b3ca316045e027902aa125976facf31ea763
ArthurAtangana <arthur.atangana@protonmail.com>
2025-03-03 17:06:03 -0500

    add cards enum

---
+++ main/include/src/shared_data/cards.hpp
@@ -0,0 +1,20 @@
+#ifndef CARDS_HPP
+#define CARDS_HPP
+
+enum class Cards {
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+};
+
+#endif
