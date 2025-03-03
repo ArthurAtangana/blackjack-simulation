90b6cc9764080a3c01db239dcab30380785daff5
orselane <alexandremarques@cmail.carleton.ca>
2025-03-03 20:20:40 +0000

    Added output port to threshold for value

--- main/include/threshold.hpp
+++ main/include/threshold.hpp
@@ -17,28 +17,37 @@ struct thresholdState{
     double sigma;
     // Defaults
 explicit thresholdState():
-    value(17), 
-    card(0),
-    sigma(infinity()) {}
+    threshold(18), // Not sure how to change this once at start of model
+    comparisonValue(0),
+    sigma(std::numeric_limits<double>::infinity()) {}
 };
+#ifndef NO_LOGGING
+std::ostream& operator<<(std::ostream &out, const thresholdState& state) {
+    out << "{threshold: " << state.threshold << ", comparisonValue: " << state.comparisonValue << "}";
+    return out;
+}
+#endif
+
 
 class threshold: public Atomic<thresholdState> {
     // Port declaration
-    public Port<int> valueIn;
-    public Port<decision> decisionOut;
+    public: Port<int> valueIn;
+    public: Port<decision> decisionOut;
+    public: Port<int> valueOut;
 
     
-    atomic_model(const std::string id) : Atomic<deck_modelState>(id, deck_modelState()) {
+    threshold(const std::string id) : Atomic<thresholdState>(id, thresholdState()) {
         //Constructor of your atomic model. Initialize ports here.
         valueIn = addInPort<int>("valueIn");
-        decisionOut = addOutPort<decisionOut>("decisionOut");
+        decisionOut = addOutPort<decision>("decisionOut");
+        valueOut = addOutPort<int>("valueOut");
     }
     
     void externalTransition(thresholdState& state, double e) const override {
         // we can read input messages from a port like this:
         if(!valueIn->empty()){
             for (const auto& x: valueIn -> getBag()) {
-                state.card = x;
+                state.comparisonValue = x;
             }
         }
         state.sigma = 0.1; // Not sure how to add variation yet.
@@ -49,15 +58,20 @@ class threshold: public Atomic<thresholdState> {
         decision res = state.threshold > state.comparisonValue ? HIT : STAND;
 
         decisionOut->addMessage(res);
+        if(res == decision::STAND){
+            valueOut->addMessage(state.comparisonValue);
+        }
     }
 
     void internalTransition(thresholdState& state) const override {
         // Reset
         state.comparisonValue = 0;
-        state.sigma = infinity();
+        state.sigma = std::numeric_limits<double>::infinity();
     }
 
     double timeAdvance(const thresholdState& state) const override {
         return state.sigma;
     }
 };
+
+#endif
