a3e78241fd9d5d7c90d83713b20aa8c03ce4d1f2
orselane <alexandremarques@cmail.carleton.ca>
2025-03-04 00:02:25 +0000

    Implement hand, with its tests.

--- main/include/src/atomic_models/hand.hpp
+++ main/include/src/atomic_models/hand.hpp
@@ -5,54 +5,70 @@ using namespace cadmium;
 
 #include <limits> //Required for infinity
 #include "cadmium/modeling/devs/atomic.hpp"
+#include "include/src/shared_data/cards.hpp"
 
 struct handState{
     int value;
-    String card;
+    Cards card;
     double sigma;
     // Defaults
 explicit handState():
     value(0), 
-    card(""),
-    sigma(infinity()) {}
+    card(Cards::NO_CARD),
+    sigma(std::numeric_limits<double>::infinity()) {}
 };
+#ifndef NO_LOGGING
+std::ostream& operator<<(std::ostream &out, const handState& state) {
+    out << "{value: " << state.value << ", card: " << state.card << "}";
+    return out;
+}
+#endif
+
+
+int cardConverter(Cards c){
+    return c > 10 ? 10 : c;
+}
+
+int nextValue(const handState& state){
+    return state.value + cardConverter(state.card);
+}
 
 class hand: public Atomic<handState> {
     // Port declaration
-    public Port</datatype/> /portName/;
+    public: 
+        Port<Cards> cardIn;
+        Port<int> handValueOut;
 
     
-    atomic_model(const std::string id) : Atomic<deck_modelState>(id, deck_modelState()) {
+        hand(const std::string id) : Atomic<handState>(id, handState()) {
         //Constructor of your atomic model. Initialize ports here.
-        /name of the port variable/ = addInPort</datatype/>("/portname/");
-        /name of the port variable/ = addOutPort</datatype/>("/portname/");
+        cardIn = addInPort<Cards>("cardIn");
+        handValueOut = addOutPort<int>("handValueOut");
     }
 
     void externalTransition(handState& state, double e) const override {
-        // we can read input messages from a port like this:
-        if(!/input port/->empty()){
-            for (const auto& x: /input port/ -> getBag()) {
-                // x is an input message!
-            }
+        if(!cardIn->empty()){
+            const auto lastInput = (cardIn -> getBag()).back();
+            state.card = lastInput;
         }
+        state.sigma = 0.1; // Not sure how to add variation yet.
+    }
+
+    void output(const handState& state) const override {
+    // Send next hand value to output
+        handValueOut->addMessage(nextValue(state));
+    }
+
+    void internalTransition(handState& state) const override {
         //your code block goes here
+        state.value = nextValue(state);
+        state.card = Cards::NO_CARD;
+        state.sigma = std::numeric_limits<double>::infinity();
     }
 
-    void output(const /model name/State& state) const override {
-    // Here, we can add message to output ports.
-        /output port/->addMessage(/value/);
-    }
-
-    void internalTransition(/model name/State& state) const override {
-        //your code block goes here
-    }
-
-    double timeAdvance(const MyAtomicState& state) const override {
+    [[nodiscard]] double timeAdvance(const handState& state) const override {
         return state.sigma;
     }
 };
 
-[[nodiscard]] double timeAdvance(const handState& state) const override {}
-//  This method defines the time advance function of your model. It can read state variables and returns the time expiry of that state.
-void confluentTransition(handState& state, double e) const override {}
-// This method defines the confluent transition function of the model. It has access to the elapsed time, input, and can read and modify the state variables
+#endif
