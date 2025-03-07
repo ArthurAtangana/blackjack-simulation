534a6004d020c75a569a47c9de25beea1ff1328f
ArthurAtangana <arthur.atangana@protonmail.com>
2025-03-07 14:47:54 -0500

    re-organise file structure and replace deck test with new file input one

--- main/include/test/test_couplings/deck_test.hpp
+++ main/include/test/test_couplings/deck_test.hpp
@@ -1,21 +1,27 @@
-#ifndef deck_test_HPP
-#define deck_test_HPP
+#ifndef DECK_TEST_HPP
+#define DECK_TEST_HPP
 
-#include "cadmium/modeling/devs/coupled.hpp"
-#include "cadmium/modeling/devs/atomic.hpp"
-#include "src/atomic_models/deck.hpp"
-#include "test/mock_models/mockController.hpp"
+#include "cadmium/modeling/devs/coupled.hpp" //cadmium header
+#include "cadmium/lib/iestream.hpp"          //iestream header
+#include "src/atomic_models/deck.hpp"        //counter atomic model file
+#include "src/shared_data/commands.hpp"
+#include "src/shared_data/players.hpp"
 
-class deck_test: public cadmium::Coupled {
-public:
-    deck_test(const std::string& id) : Coupled(id) {
-        // Create deck model instance
-        auto deckModel = addComponent<deck>("deck");
-        auto mockControllerModel = addComponent<mockController>("mockController");
-        // couplings
-        addCoupling(mockControllerModel->commandOutPort, deckModel->commandInPort);
-        addCoupling(mockControllerModel->playerOutPort, deckModel->playerInPort);
-    }
+using namespace cadmium;
+
+struct deck_test: public Coupled {
+
+  deck_test(const std::string& id): Coupled(id){
+    auto deckModel = addComponent<deck>("deck model");
+    auto deck_commands = addComponent<lib::IEStream<deckCommand>>("deck commands file", "main/include/test/inputs/deck_inputs/deck_commands_test_1.txt");
+    auto deck_players = addComponent<lib::IEStream<Players>>("deck players file", "main/include/test/inputs/deck_inputs/deck_players_test_1.txt");
+
+    //Internal Couplings
+    addCoupling(deck_commands->out, deckModel->commandInPort);
+    addCoupling(deck_players->out, deckModel->playerInPort);
+  }
+
 };
+
 
 #endif
--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,5 @@
 #include <limits>
 #include "include/test/test_couplings/controller_test.hpp"
-#include "include/test/test_couplings/deck_file_test.hpp"
 #include "include/test/test_couplings/thresholdC_test.hpp"
 #include "include/test/test_couplings/thresholdD_test.hpp"
 #include "include/test/test_couplings/challenger_test.hpp"
@@ -52,12 +51,11 @@ extern "C" {
 		// auto model = std::make_shared<handTest> ("hand_test");
 		// auto model = std::make_shared<challengerTest> ("challengerTest");
 		// auto model = std::make_shared<dealerTest> ("dealerTest");
-		// auto model = std::make_shared<deck_test> ("deck_test");
+		auto model = std::make_shared<deck_test> ("deck_test");
 		// auto model = std::make_shared<handTest> ("hand_test");
 		// auto model = std::make_shared<challenger> ("challenger");
 		// auto model = std::make_shared<playersTest> ("players_test");
 		// auto model = std::make_shared<controllerTest> ("controllerTest");
-		auto model = std::make_shared<deck_input_test> ("deck_input_test");
 
 		
 		#ifdef SIM_TIME
