26e7f115e0a38c364346529a7551e007fb7ba4df
ArthurAtangana <arthur.atangana@protonmail.com>
2025-03-09 18:26:31 -0400

    Add build logic for controller executable

--- main/include/test/test_couplings/controller_test.hpp
+++ main/include/test/test_couplings/controller_test.hpp
@@ -1,22 +1,21 @@
-#ifndef TEST_CONTROLLER_HPP
-#define TEST_CONTROLLER_HPP
+#ifndef CONTROLLER_TEST_HPP
+#define CONTROLLER_TEST_HPP
 
-#include "cadmium/modeling/devs/coupled.hpp"
-// #include "cadmium/modeling/devs/atomic.hpp"
+
+#include "cadmium/modeling/devs/coupled.hpp" //cadmium header
 #include "cadmium/lib/iestream.hpp"          //iestream header
 #include "src/atomic_models/controller.hpp"
-#include "test/mock_models/mock_game.hpp"
+#include "src/shared_data/commands.hpp"
 #include "src/shared_data/commands.hpp"
 
 using namespace cadmium;
 
-class controllerTest: public cadmium::Coupled {
+class controller_test: public cadmium::Coupled {
 public:
-    controllerTest(const std::string& id) : Coupled(id) {
+    controller_test(const std::string& id) : Coupled(id) {
         //Create model instances
         
         auto controllerModel = addComponent<controller>("controller");
-        // auto mockGameModel = addComponent<mock_game>("mock_game");
         auto hand_value = addComponent<lib::IEStream<int>>("hand_value_lose", "main/include/test/inputs/controller_inputs/controller_hand_value_lose_input.txt");
         auto decision_input = addComponent<lib::IEStream<decision>>("decision_1", "main/include/test/inputs/controller_inputs/controller_decision_input_1.txt");
         auto start = addComponent<lib::IEStream<int>>("start_input", "main/include/test/inputs/controller_inputs/controller_start_input.txt");
@@ -25,9 +24,6 @@ public:
         addCoupling(start->out, controllerModel->startInPort);
         addCoupling(decision_input->out, controllerModel->decisionInPort);
         addCoupling(hand_value->out, controllerModel->handValueInPort);
-        // addCoupling(mockGameModel->startPort, controllerModel->startInPort);
-        // addCoupling(mockGameModel->decisionOutPort, controllerModel->decisionInPort);
-        // addCoupling(mockGameModel->handValueOutPort, controllerModel->handValueInPort);
     }
 };
 #endif
