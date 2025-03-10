a737d1456ef5fffe5a031eb37bbfbe3832b10ce1
ArthurAtangana <arthur.atangana@protonmail.com>
2025-03-10 16:51:58 -0400

    Refactor main to be simpler and add logging

--- main/TestCases/threshold_d_test.cpp
+++ main/TestCases/threshold_d_test.cpp
@@ -1,69 +1,20 @@
 #include <limits>
 #include "include/test/test_couplings/thresholdD_test.hpp"
-
-/*
-There are 3 macros defined at compile time that changes the behaviour of the simulation.
---> SIM_TIME: This macro, when defined, runs the simulation in simulation time. Else, the simulation runs at wall clock.
---> ESP_PLATFORM: When defined, the models are compiled for the ESP32 microcontroller. Else, compiles for Linux/ Windows
---> NO_LOGGING: When defined, prevents logging (maybe useful in embedded situations)
-*/
-
-
-#ifdef SIM_TIME
-	#include "cadmium/simulation/root_coordinator.hpp"
-#else
-	#include "cadmium/simulation/rt_root_coordinator.hpp"
-	#ifdef ESP_PLATFORM
-		#include <cadmium/simulation/rt_clock/ESPclock.hpp>
-	#else
-		#include <cadmium/simulation/rt_clock/chrono.hpp>
-	#endif
-#endif
-
-#ifndef NO_LOGGING
-	#include "cadmium/simulation/logger/stdout.hpp"
-	#include "cadmium/simulation/logger/csv.hpp"
-#endif
+#include "cadmium/simulation/root_coordinator.hpp"
+#include "cadmium/simulation/logger/stdout.hpp"
+#include "cadmium/simulation/logger/csv.hpp"
 
 using namespace cadmium;
 
-extern "C" {
-	#ifdef ESP_PLATFORM
-		void app_main() //starting point for ESP32 code
-	#else
-		int main()		//starting point for simulation code
-	#endif
-	{
+int main() {		//starting point for simulation code
 	
-		auto model = std::make_shared<ThresholdDTest>("threshold_d_test");
+	auto model = std::make_shared<ThresholdDTest>("threshold_d_test");
+	auto rootCoordinator = RootCoordinator(model);
+	// rootCoordinator.setLogger<STDOUTLogger>(";");
+	rootCoordinator.setLogger<CSVLogger>("logs/threshold_d_test.csv", ";");
 
-		
-		#ifdef SIM_TIME
-			auto rootCoordinator = cadmium::RootCoordinator(model);
-		#else
-			#ifdef ESP_PLATFORM
-				cadmium::ESPclock clock;
-				auto rootCoordinator = cadmium::RealTimeRootCoordinator<cadmium::ESPclock<double>>(model, clock);
-			#else
-				cadmium::ChronoClock clock;
-				auto rootCoordinator = cadmium::RealTimeRootCoordinator<cadmium::ChronoClock<std::chrono::steady_clock>>(model, clock);
-			#endif
-		#endif
-
-		#ifndef NO_LOGGING
-		rootCoordinator.setLogger<STDOUTLogger>(";");
-		#endif
-
-		rootCoordinator.start();
-		#ifdef ESP_PLATFORM
-			rootCoordinator.simulate(std::numeric_limits<double>::infinity());
-		#else
-			rootCoordinator.simulate(30.0); // Max time limit
-		#endif
-		rootCoordinator.stop();	
-
-		#ifndef ESP_PLATFORM
-			return 0;
-		#endif
-	}
+	rootCoordinator.start();
+	rootCoordinator.simulate(30.0); // Max time limit
+	rootCoordinator.stop();	
+	return 0;
 }
