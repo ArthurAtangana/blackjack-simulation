9d80484d5ba405764d1774ffa5d4b1f2f835040d
orselane <alexandremarques@cmail.carleton.ca>
2025-03-10 18:25:51 +0000

    Added in override for cards

--- main/include/test/test_couplings/hand_test_illegal.hpp
+++ main/include/test/test_couplings/hand_test_illegal.hpp
@@ -5,13 +5,14 @@
 #include "cadmium/modeling/devs/atomic.hpp"
 #include "src/atomic_models/hand.hpp"
 #include "cadmium/lib/iestream.hpp"          //iestream header
+#include "src/shared_data/cards.hpp"
 
 class handTestIllegal: public cadmium::Coupled {
 public:
 handTestIllegal(const std::string& id) : Coupled(id) {
         // Create model instances
         auto handModel = addComponent<hand>("hand");
-        auto mockModel = addComponent<lib::IEStream<int>>("hand input", "main/include/test/inputs/hand_inputs/hand_in_illegal.txt");
+        auto mockModel = addComponent<lib::IEStream<Cards>>("hand input", "main/include/test/inputs/hand_inputs/hand_in_illegal.txt");
 
         addCoupling(mockModel->out, handModel->cardIn);
     }
