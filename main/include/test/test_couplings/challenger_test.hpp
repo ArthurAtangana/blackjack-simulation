ab03413a5ab89c6ea839c3bf4baeaa3e9c7a7161
orselane <alexandremarques@cmail.carleton.ca>
2025-03-10 18:42:52 +0000

    Updated test couplings

--- main/include/test/test_couplings/challenger_test.hpp
+++ main/include/test/test_couplings/challenger_test.hpp
@@ -3,17 +3,18 @@
 
 #include "cadmium/modeling/devs/coupled.hpp"
 #include "cadmium/modeling/devs/atomic.hpp"
-#include "test/mock_models/mock_deck.hpp"
 #include "src/coupled_models/challenger.hpp"
+#include "cadmium/lib/iestream.hpp"          //iestream header
+#include "src/shared_data/cards.hpp"
 
 class challengerTest: public cadmium::Coupled {
 public:
     challengerTest(const std::string& id) : Coupled(id) {
         // Create model instances
         auto challengerModel = addComponent<challenger>("challenger");
-        auto mockModel = addComponent<mockDeck>("mockDeck");
+        auto mockModel = addComponent<lib::IEStream<Cards>>("challenger in", "main/include/test/inputs/player_in/challenger_in.txt");
 
-        addCoupling(mockModel->mockOut, challengerModel->challengerCardIn);
+        addCoupling(mockModel->out, challengerModel->challengerCardIn);
     }
 };
 
--- main/include/test/test_couplings/dealer_test.hpp
+++ main/include/test/test_couplings/dealer_test.hpp
@@ -3,17 +3,18 @@
 
 #include "cadmium/modeling/devs/coupled.hpp"
 #include "cadmium/modeling/devs/atomic.hpp"
-#include "test/mock_models/mock_deck.hpp"
 #include "src/coupled_models/dealer.hpp"
+#include "cadmium/lib/iestream.hpp"          //iestream header
+#include "src/shared_data/cards.hpp"
 
 class dealerTest: public cadmium::Coupled {
 public:
     dealerTest(const std::string& id) : Coupled(id) {
         // Create model instances
         auto dealerModel = addComponent<dealer>("dealer");
-        auto mockModel = addComponent<mockDeck>("mockDeck");
+        auto mockModel = addComponent<lib::IEStream<Cards>>("dealer in", "main/include/test/inputs/player_in/dealer_in.txt");
 
-        addCoupling(mockModel->mockOut, dealerModel->dealerCardIn);
+        addCoupling(mockModel->out, dealerModel->dealerCardIn);
     }
 };
 
