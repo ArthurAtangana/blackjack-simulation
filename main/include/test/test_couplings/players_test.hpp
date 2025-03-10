bf661558e0b9fba9f496213f56d228fb69d92bc0
orselane <alexandremarques@cmail.carleton.ca>
2025-03-10 19:13:44 +0000

    Fixed input filepath

--- main/include/test/test_couplings/players_test.hpp
+++ main/include/test/test_couplings/players_test.hpp
@@ -3,7 +3,6 @@
 
 #include "cadmium/modeling/devs/coupled.hpp"
 #include "cadmium/modeling/devs/atomic.hpp"
-#include "test/mock_models/mock_deck.hpp"
 #include "src/coupled_models/players.hpp"
 #include "cadmium/lib/iestream.hpp"          //iestream header
 #include "src/shared_data/cards.hpp"
@@ -14,7 +13,7 @@ public:
         // Create model instances
         auto playersModel = addComponent<players>("players");
         auto dealerIn = addComponent<lib::IEStream<Cards>>("dealer cards", "main/include/test/inputs/player_inputs/dealer_in.txt");
-        auto chalIn = addComponent<lib::IEStream<Cards>>("challenger cards", "main/include/test/inputs/threshold_inputs/challenger_in.txt");
+        auto chalIn = addComponent<lib::IEStream<Cards>>("challenger cards", "main/include/test/inputs/player_inputs/challenger_in.txt");
 
         addCoupling(dealerIn->out, playersModel->dealerIn);
         addCoupling(chalIn->out, playersModel->challengerIn);
